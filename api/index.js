28c7ae64df6c3baf50417bdb44eb65e11fb19740
ALBERT IMASUEN <studentxapp@gmail.com>
2025-12-24 17:32:38 -0500

    Adding configurations

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ import userRoutes from './routes/user.route.js'
 import authRoutes from './routes/auth.route.js'
 import postRoutes from './routes/post.route.js'
 import commentRoutes from './routes/comment.route.js'
+import path from 'path'
 
 dotenv.config();
 
@@ -15,13 +16,14 @@ mongoose.connect(process.env.MONGO).then(() =>{
     console.log(err)
 })
 
+const __dirname = path.resolve();
+
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
+
 
 //Creating Routes
 
@@ -29,3 +31,19 @@ app.use('/api/user', userRoutes)
 app.use('/api/auth', authRoutes)
 app.use('/api/post', postRoutes)
 app.use('/api/comment', commentRoutes)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+// app.get('*', (req, res) =>{
+//     res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+// });
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+});
+
+// START SERVER
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
