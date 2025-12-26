405d6d489543191fd011dfa41332e9013b2c5ba9
ALBERT IMASUEN <studentxapp@gmail.com>
2025-12-26 14:14:43 -0500

    Addme23

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { deleteUser, getUser, getUsers, signout, test, updateuser, updateUserStatus } from '../controllers/user.controller.js';
+import { deleteUser, getUser, getUsers, signout, test, updateUser, updateuser, updateUserStatus } from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test', test)
-router.put('/update/:userId', updateuser);
+router.put('/update/:userId', updateUser);
+// router.put('/update/:userId', updateuser);
 router.put('/updateUserStatus/:userId', updateUserStatus);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.get('/getusers', verifyToken, getUsers);
